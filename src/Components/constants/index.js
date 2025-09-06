ac479b74ffabc6111820e234076e7a0af20855a5
hamadouche-zahra <zahrahamadouche8@gmail.com>
2025-09-06 20:46:23 +0200

    feat: add navigation links data for site sections

---
+++ src/Components/constants/index.js
@@ -0,0 +1,26 @@
+export const navLinksdata=[
+    {
+        _id:1001,
+        title:"Accueil",
+        link:"home",
+    },
+    {
+        _id:1002,
+        title:"À propos",
+        link:"about",
+    },
+
+    
+    {
+        _id:1004,
+        title:"Résumé",
+        link:"resume",
+    },
+    {
+        _id:1005,
+        title:"Contact",
+        link:"contact",
+    },
+    
+
+]
