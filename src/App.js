70ebdf1f77ee22f1b7c6ce02309666b8c968a05f
hamadouche-zahra <zahrahamadouche8@gmail.com>
2025-09-06 20:40:27 +0200

    init: add React + Tailwind base setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Banner from './Components/banner/Banner'
+import Resume from './Components/resume/Resume'
+import Contact from './Components/contact/Contact'
+import Footer from './Components/footer/Footer'
+import About from './Components/about/About'
+
+
+function App() {
+  return (
+    <div className='w-full h-auto bg-bodyColor text-lightText'>
+      <div className='max-w-screen-2xl mx-auto px-16'>
+      <Navbar/>  
+      <Banner/>
+      <About/>
+      <Resume/>
+      <Contact/>
+      <Footer/>
+
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
